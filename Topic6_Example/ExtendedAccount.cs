ddbe851534c41d8cc5c53f7d9337f4652a2a82ad
KyawLinThant <kyawlinthant25@gamil.com>
2023-12-21 09:59:36 +0630

    C#ATM

---
+++ Topic6_Example/ExtendedAccount.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Topic6_Example
+{
+    [Serializable()]
+    public class ExtendedAccount:Account , ISerializable   
+    {
+        private double interestrate;
+
+        public double InterestRate;
+
+        public ExtendedAccount(string a, string p, int b, double i):base(a, p, b)
+        {
+            this.InterestRate = i;
+        }
+        public ExtendedAccount(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+            this.InterestRate = (double)info.GetValue("InterestRate", typeof(double));
+
+        }
+
+        public override void getInterest()
+        {
+            double interest = this.Balance * this.InterestRate;
+            this.Balance =this.Balance+(int)interest;
+        }
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+            info.AddValue("InterestRate", this.InterestRate);
+        }
+
+        public override string queryType()
+        {
+            return "Extended";
+        }
+    }
+}
---
+++ Topic6_Example/SerializedClass.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Topic6_Example
+{
+    [Serializable()]
+    public class SerializedClass : Account, ISerializable
+    {
+        private string name;
+        private string address;
+        private string email;
+
+
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }   
+
+        public SerializedClass(string an, string p, int b,string name, string address, string email): base(an,p, b) 
+        {
+            this.Name = name;
+            this.Address = address;
+            this.Email = email;
+        }
+
+        public SerializedClass(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+            this.Name = (string)info.GetValue("Name", typeof(string));
+            this.Address = (string)info.GetValue("Address", typeof(string));
+            this.Email = (string)info.GetValue("Email", typeof(string));
+        }
+
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+            info.AddValue("Name", this.Name);
+            info.AddValue("Address", this.Address);
+            info.AddValue("Email", this.Email);
+        }
+
+
+    }
+}
